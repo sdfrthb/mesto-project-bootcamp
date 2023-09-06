a668a7bafa048f9c053ecaf9b6a882d7f1f6fe95
Tanya Zakharova <sdfrthb23@yandex.ru>
2023-09-06 17:22:53 +0500

    validation fix

--- src/components/validate.js
+++ src/components/validate.js
@@ -1,33 +1,30 @@
-validationSelectors = {
+ const validationSelectors = {
   formSelector: '.popup__form',
   inputSelector: '.popup__input',
   submitButtonSelector: '.popup__save-button',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible'
 }
-
 
 function showError(inputElement, errorText) {
   const errorElement = document.getElementById(`${inputElement.id}-error`);
   errorElement.textContent = errorText;
-  inputElement.classList.add('popup_opened');
+  inputElement.classList.add('popup__input_type_error');
 };
 
 function hideError(inputElement) {
   const errorElement = document.getElementById(`${inputElement.id}-error`);
   errorElement.textContent = '';
-  inputElement.classList.add('popup_opened');
+  inputElement.classList.remove('popup__input_type_error');
 };
 
-function showErrorLine(inputElement, selectors) {
-  inputElement.classList.add(selectors.inputErrorClass);
-};
+function checkValid(inputElement) {
+  if (!inputElement.validity.valid) {
+    showError(inputElement, inputElement.validationMessage);
+  } else {
+        hideError(inputElement);
+  }
+}
 
-function hideErrorLine(inputElement, selectors) {
-  inputElement.classList.remove(selectors.inputErrorClass);
-};
-
-function unableButton(button) {
+function enableButton(button) {
   button.disabled = false;
 };
 
@@ -35,37 +32,29 @@ function disableButton(button) {
   button.disabled = true;
 };
 
-function checkValid(inputElement) {
-  if (!inputElement.validity.valid) {
-    showError(inputElement, inputElement.validationMessage);
-    showErrorLine(inputElement,selectors);
-  } else {
-    hideError(inputElement);
-    hideErrorLine(inputElement,selectors);
-  }
-};
-
-function checkButton(form, button) {
-  if (form.checkValidity()) {
-    unableButton(button);
+function checkButton(formElement, button) {
+  if (formElement.checkValidity()) {
+    enableButton(button);
   } else {
     disableButton(button);
   }
 };
 
-function enableValidation(selectors) {
-  const formList = document.querySelectorAll(selectors.formSelector);
-  const saveButtonList = document.querySelectorAll(selectors.submitButtonSelector);
-  disableButton(saveButtonList[1]);
-  formList.forEach((formElement) => {
-    setEventListeners(formElement, selectors);
+function setEventListeners(formElement) {
+  const inputList = formElement.querySelectorAll(validationSelectors.inputSelector);
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', () => {
+      checkValid(inputElement);
+      checkButton(formElement, formElement.lastElementChild)
+    })
   });
 };
 
-function setEventListeners(formElement, selectors) {
-  const inputList = formElement.querySelectorAll(selectors.inputSelector);
-  inputList.forEach(inputElement => inputElement.addEventListener('input', () => {
-    checkValid(inputElement);
-    checkButton(formElement, formElement.lastElementChild)
-  }));
-}
+function enableValidation() {
+  const formList = document.querySelectorAll(validationSelectors.formSelector);
+  const saveButtonList = document.querySelectorAll(validationSelectors.submitButtonSelector);
+  disableButton(saveButtonList[1]);
+  formList.forEach((formElement) => setEventListeners(formElement));
+};
+
+export {enableValidation}
--- src/index.js
+++ src/index.js
@@ -1,40 +1,8 @@
-// const arhiz = new URL('https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg', import.meta.url);
-// const chelyabinskayaObl = new URL('https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg', import.meta.url);
-// const ivanovo = new URL('https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg', import.meta.url);
-// const kamchatka = new URL('https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg', import.meta.url);
-// const holmgorskiiRayon = new URL('https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg', import.meta.url);
-// const baikal = new URL('https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg', import.meta.url);
 
-// const initialCards = [
-//   {
-//     name: 'Архыз',
-//     link: arhiz
-//   },
-//   {
-//     name: 'Челябинская область',
-//     link: chelyabinskayaObl
-//   },
-//   {
-//     name: 'Иваново',
-//     link: ivanovo
-//   },
-//   {
-//     name: 'Камчатка',
-//     link: kamchatka
-//   },
-//   {
-//     name: 'Холмогорский район',
-//     link: holmgorskiiRayon
-//   },
-//   {
-//     name: 'Байкал',
-//     link: baikal
-//   }
-// ];
 import './pages/index.css';
 import {closePopup, openPopup, overlayClose} from './components/modal.js';
 import {createCard, addCard} from './components/card.js';
-import {enableValidation, validationSelectors} from './components/validate.js';
+import {enableValidation} from './components/validate.js';
 
 
 const initialCards = [
@@ -141,7 +109,7 @@ closeButtons.forEach((button) => {
 editForm.addEventListener('submit', submitEditForm);
 addForm.addEventListener('submit', submitAddForm);
 
-enableValidation(validationSelectors);
+enableValidation();
 
 
 
